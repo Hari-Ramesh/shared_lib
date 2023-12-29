33f71f61677c9c753f0e8e7454ff7026e546ec03
Hari-Ramesh <120625038+Hari-Ramesh@users.noreply.github.com>
2023-12-29 11:53:57 +0530

    Update src.groovy

--- vars/src.groovy
+++ vars/src.groovy
@@ -1,1 +1,14 @@
-hello
+def call (def giturl, def branch) {
+  echo ""
+  echo "---------- Git URL: $(giturl) ----------
+  echo "---------- Git URL: $(branch) ----------
+  echo ""
+  if (giturl.contains("github.com"))
+  {
+  checkout ([$class: 'GitSCM', (branches: [[name: 'branch']], extensions: [], userRemoteConfigs: [[url: 'giturl']]]) 
+  }
+  else 
+  {
+    error("please provider valid github")
+  }
+}
