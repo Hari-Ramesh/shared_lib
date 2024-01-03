b2ef285095edebe316236ab720298044158068a5
Hari-Ramesh <120625038+Hari-Ramesh@users.noreply.github.com>
2024-01-03 12:27:37 +0530

    Update src.groovy

--- vars/src.groovy
+++ vars/src.groovy
@@ -1,12 +1,14 @@
 def call(def giturl, def branch) {
-
-  // Use println for output in Groovy:
-  println "---------- Git URL: $giturl ----------"
-  println "---------- Branch: $branch ----------"
-
-  if (giturl.contains("github.com")) {
-    checkout([$class: 'GitSCM', branches: [[name: branch]], extensions: [], userRemoteConfigs: [[url: giturl]]])
-  } else {
-    error("Please provide a valid GitHub URL")
+   
+  sh "echo ---------- Git URL: $(giturl) ----------"
+  sh "echo ---------- Git URL: $(branch) ----------"
+  
+  if (giturl.contains("github.com"))
+  {
+  checkout ([$class: 'GitSCM', (branches: [[name: 'branch']], extensions: [], userRemoteConfigs: [[url: 'giturl']]]) 
+  }
+  else 
+  {
+    error("please provider valid github")
   }
 }
