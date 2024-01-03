62c67ace372fc81f0cad30b86f9984448089645a
Hari-Ramesh <120625038+Hari-Ramesh@users.noreply.github.com>
2024-01-03 12:48:44 +0530

    Update src.groovy

--- vars/src.groovy
+++ vars/src.groovy
@@ -1,6 +1,8 @@
 def call (def giturl, def branch) {
+  sh "echo"
   sh "echo ---------- Git URL: ${giturl} ----------"
   sh "echo ---------- Git URL: ${branch} ----------"
+  sh "echo"
   if (giturl.contains("github.com"))
   {
   checkout ([$class: 'GitSCM', branches: [[name: branch]], extensions: [], userRemoteConfigs: [[url: giturl]]]) 
